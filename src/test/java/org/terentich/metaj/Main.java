3bcf2fdf5176dc1432f334db70c3ca39eba18051
Alexey V. Terentyev <terentich@gmail.com>
2012-10-08 23:33:57 +0400

    Add Javadoc-information in example, test class.

--- src/test/java/org/terentich/metaj/Main.java
+++ src/test/java/org/terentich/metaj/Main.java
@@ -2,26 +2,40 @@ package org.terentich.metaj;
 
 import java.io.IOException;
 
+/**
+ * The example class for testing application.
+ *
+ * @author Alexey V. Terentyev
+ * @date 08.10.2012
+ */
 public class Main {
-	public static void main(String[] args) throws IOException {
-		MetaReader reader = new MetaReader(Main.class);
-		System.out.println(reader);
-		System.out.println("Archiver version: " + reader.getArchiverVersion());
-		System.out.println("Build-Jdk: " + reader.getBuildJdk());
-		System.out.println("Built-By: " + reader.getBuiltBy());
-		System.out.println("Classpath: " + reader.getClasspath());
-		System.out.println("Created-By: " + reader.getCreatedBy());
-		System.out.println("Impl title: " + reader.getImplementationTitle());
-		System.out.println("Impl vendorId: "
-				+ reader.getImplementationVendorId());
-		System.out
-				.println("Impl version: " + reader.getImplementationVersion());
-		System.out.println("Mainclass: " + reader.getMainClass());
-		System.out.println("Manifest version: " + reader.getManifestVersion());
-		System.out.println("Spec title: " + reader.getSpecificationTitle());
-		System.out.println("Spec version: " + reader.getSpecificationVersion());
+    /**
+     * The main entry point for example application.
+     *
+     * @param args
+     *            the input arguments
+     * @throws IOException
+     *             if some I/O error occurs
+     */
+    public static void main(String[] args) throws IOException {
+        MetaReader reader = new MetaReader(Main.class);
+        System.out.println(reader);
+        System.out.println("Archiver version: " + reader.getArchiverVersion());
+        System.out.println("Build-Jdk: " + reader.getBuildJdk());
+        System.out.println("Built-By: " + reader.getBuiltBy());
+        System.out.println("Classpath: " + reader.getClasspath());
+        System.out.println("Created-By: " + reader.getCreatedBy());
+        System.out.println("Impl title: " + reader.getImplementationTitle());
+        System.out.println("Impl vendorId: "
+                + reader.getImplementationVendorId());
+        System.out
+                .println("Impl version: " + reader.getImplementationVersion());
+        System.out.println("Mainclass: " + reader.getMainClass());
+        System.out.println("Manifest version: " + reader.getManifestVersion());
+        System.out.println("Spec title: " + reader.getSpecificationTitle());
+        System.out.println("Spec version: " + reader.getSpecificationVersion());
 
-		System.out.println("Impl timestamp: "
-				+ reader.getImplementationTimestamp());
-	}
+        System.out.println("Impl timestamp: "
+                + reader.getImplementationTimestamp());
+    }
 }
